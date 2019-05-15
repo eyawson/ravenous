bc8030f5a9476b566c1972a5f9eeed83e3ad2432
eyawson <ebow.yawson@gmail.com>
2019-05-14 21:44:42 -0400

    update sort option to return className for styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -25,10 +25,16 @@ class SearchBar extends React.Component {
         }
     }
 
+    handleSortByChange(sortByOption) {
+        this.setState({
+            sortBy: sortByOption
+        })
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         });
     }
 
