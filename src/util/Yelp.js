ba9f18acc99721dc08efb33ef04faa7b28890b69
eyawson <ebow.yawson@gmail.com>
2019-05-19 21:03:16 -0400

    complete yelp json response mapped object

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,30 @@
 const apiKey = 'yAZZN8_MprHKsWNQmmvdsZWwN6zMrq0NwbDw0kWmWGlzzOgRTRIQ5xcrcC0Pznq2ePMXH-cietb3hgltla8s_bM9QhnR3LQcQ0Ob_bRhmGVetfiRc2uQ3DWo-O_hXHYx';
 
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: { Authorization: `Bearer ${apiKey}`}
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.bussinesses.map(business => {
+                    return {
+                        id: business.id,
+                        imagSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                })
+            }
+        });
+    }
+};
+
+export default Yelp;
